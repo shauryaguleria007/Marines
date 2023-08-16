03a7e33e25f936074618475889f9f21dd28d9838
shaurya guleria <shauryaguleria12345678@gmail.com>
2023-08-16 16:42:15 +0530

    order error resolved

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -8,10 +8,10 @@ const { UserModal } = require("../modal/userModal")
 exports.createOrder = RouterAsyncErrorHandler(async (req, res, next) => {
     const user = req.user
     const cart = user.cart
-    cart.data.map(async (cartOrder) => {
+    await Promise.all(cart.data.map(async (cartOrder) => {
         const product = await ProductModal.findById(cartOrder.product)
         if (!product) throw new Error()
-        if (product?.stock < cartOrde?.quantity) throw new Error()
+        if (product?.stock < cartOrder?.quantity) throw new Error()
         const order = await OrderModal.create({
             user: user.id,
             seller: product.seller,
@@ -22,7 +22,7 @@ exports.createOrder = RouterAsyncErrorHandler(async (req, res, next) => {
         if (!order) throw new Error()
 
         await product.updateOne({ stock: product.stock - order.quantity, sale: product.sale + order.quantity })
-    })
+    }))
 
     await user.updateOne({
         $unset: { cart: {} }
